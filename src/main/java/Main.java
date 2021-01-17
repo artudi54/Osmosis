301ff30e205cb487d46207d636e912399ea4b5b4
pawek001 <58525337+pawek001@users.noreply.github.com>
2021-01-17 21:37:42 +0100

    Dodane nowy scenariusz przenoszenia mechanizmu stężeń, w klasie Main dodane możliwości do otwierania symulatora w zalezności od podanego znaku na konsolę

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,28 @@ import simulation.Simulator;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        URL config = Main.class.getClassLoader().getResource("configuration.json");
+        System.out.println("Wybierz, który symulator uruchomić: c - chmura, f- fabryka");
+        URL config;
+        Scanner s = new Scanner(System.in);
+        char key = s.next().charAt(0);
+        switch(key){
+            case 'f':
+            System.out.println("Włączono symulację fabryki");
+            config = Main.class.getClassLoader().getResource("configuration.json");
+            break;
+            case 'c':
+            System.out.println("Włączono symulację chmury");
+            config = Main.class.getClassLoader().getResource("cloud.json");
+            break;
+            default:
+            System.out.println("Włączono symulację fabryki");
+            config = Main.class.getClassLoader().getResource("configuration.json");
+        }
+        
         Configuration configuration = Configuration.loadFrom(config);
         System.out.println(configuration);
         
